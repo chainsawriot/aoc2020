d5db3007055e1151804e9cea56cdfd1417a54d44
chainsawriot <chainsawtiney@gmail.com>
2020-12-04 10:26:41 +0100

    day 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# AOC 2020 (Rcpp)
+
+I mean, why not?
+
+1. I will use the least amount of R possible. Probably, I will only use R for purifying data.
+2. Use C++ but in the least effort possible.
+
+R code is embedded also in the C++ code. Running the Rcpp code with
+
+```r
+Rcpp::sourceCpp("d1.cpp")
+```
---
+++ d1.cpp
@@ -0,0 +1,39 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+int d1_1(NumericVector input) {
+    for (NumericVector::iterator i = input.begin(); i != input.end(); ++i) {
+	for (NumericVector::iterator j = input.begin(); j != input.end(); ++j) {
+	    if (*i + *j == 2020) {
+		return (*i) * (*j);
+	    }
+	}
+    }
+    return NA_REAL;
+}
+
+// [[Rcpp::export]]
+int d1_2(NumericVector input) {
+    for (NumericVector::iterator i = input.begin(); i != input.end(); ++i) {
+	for (NumericVector::iterator j = input.begin(); j != input.end(); ++j) {
+	    for (NumericVector::iterator k = input.begin(); k != input.end(); ++k) {
+		if (*i + *j + * k == 2020) {
+		    return (*i) * (*j) * (*k);
+		}
+	    }
+	}
+    }
+    return NA_REAL;
+}
+
+/***R
+# make sure it match the demo input
+
+d1_1(c(1721, 979, 366, 299, 675, 1456)) == 514579
+d1_2(c(1721, 979, 366, 299, 675, 1456)) == 241861950
+
+input1 <- as.numeric(readLines(here::here("data/input1.txt")))
+d1_1(input1)
+d1_2(input1)
+*/
