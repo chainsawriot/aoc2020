d5ad86905c5167a9a5875355912ab4d1ea8c4b05
chainsawriot <chainsawtiney@gmail.com>
2020-12-17 00:45:35 +0100

    tired

--- d16.R
+++ d16.R
@@ -1,10 +1,10 @@
+Rcpp::sourceCpp("d16.cpp")
+
 input16 <- readLines(here::here("data/input16.txt"))
 criteria <- input16[1:20]
 nbtix <- input16[26:length(input16)]
 yrtix <- input16[23]
 
-Rcpp::sourceCpp("d16.cpp")
-pp(criteria[3])
 
 testdata <- "class: 1-3 or 5-7
 row: 6-11 or 33-44
@@ -34,6 +34,19 @@ stopifnot(filter_tix(testtix, testcrit) == "7,3,47")
 valid_tix <- filter_tix(nbtix, criteria)
 
 ## 190x20
-
+require(stringr)
 nb_matrix <- matrix(as.numeric(str_split(paste0(valid_tix, collapse=","), ",")[[1]]), nrow = 190, ncol = 20, byrow = TRUE)
 
+test_mat(nb_matrix, criteria)
+
+testdata_crit <- c("class: 0-1 or 4-19", "row: 0-5 or 8-19", "seat: 0-13 or 16-19")
+
+testdata_nb <- "3,9,18
+15,1,5
+5,14,9"
+
+test_matrix <- matrix(as.numeric(str_split(paste0(testdata_nb, collapse=","), "[,\n]")[[1]]), nrow = 3, ncol = 3, byrow = TRUE)
+
+test_mat(test_matrix, testdata_crit, 0)
+
+test_mat(test_matrix, criteria)
--- d16.cpp
+++ d16.cpp
@@ -5,6 +5,12 @@
 using namespace Rcpp;
 // [[Rcpp::plugins("cpp11")]]
 
+struct criterion {
+    std::string name;
+    std::pair<int, int> range1;
+    std::pair<int, int> range2;
+};
+
 
 std::vector<std::string> explode(std::string s, std::string regex_string = "[^- :]+") {
     // reinventing strsplit()
@@ -19,42 +25,87 @@ std::vector<std::string> explode(std::string s, std::string regex_string = "[^- 
 }
 
 
-std::vector<std::pair<int, int>> parse_criteria (std::vector<std::string> criteria) {
-    std::vector<std::pair<int, int>> res;
+// std::vector<std::pair<int, int>> parse_criteria (std::vector<std::string> criteria) {
+//     std::vector<std::pair<int, int>> res;
+//     for (std::string& x: criteria) {
+// 	std::vector<std::string> limits = explode(x, "[0-9]+");
+// 	std::pair<int, int> x1;
+// 	x1.first = std::stoi(limits[0]);
+// 	x1.second = std::stoi(limits[1]);
+// 	std::pair<int, int> x2;
+// 	x2.first = std::stoi(limits[2]);
+// 	x2.second = std::stoi(limits[3]);
+// 	res.push_back(x1);
+// 	res.push_back(x2);
+//     }
+//     return res;
+// }
+
+std::vector<struct criterion> parse_criteria (std::vector<std::string> criteria) {
+    std::vector<struct criterion> res;
     for (std::string& x: criteria) {
 	std::vector<std::string> limits = explode(x, "[0-9]+");
+	std::vector<std::string> words = explode(x, "[a-z ]+");
+	struct criterion crit;
+	crit.name = words[0];
 	std::pair<int, int> x1;
 	x1.first = std::stoi(limits[0]);
 	x1.second = std::stoi(limits[1]);
 	std::pair<int, int> x2;
 	x2.first = std::stoi(limits[2]);
 	x2.second = std::stoi(limits[3]);
-	res.push_back(x1);
-	res.push_back(x2);
+	crit.range1 = x1;
+	crit.range2 = x2;
+	res.push_back(crit);
     }
     return res;
 }
+
 
 bool check_value (int input, std::pair<int, int> crit) {
     return input >= crit.first && input <= crit.second;
 }
 
-bool check_value2 (int input, std::vector<std::pair<int, int>> criteria) {
-    for (std::pair<int, int>& crit: criteria) {
-	if (check_value(input, crit)) {
+// bool check_value2 (int input, std::vector<std::pair<int, int>> criteria) {
+//     for (std::pair<int, int>& crit: criteria) {
+// 	if (check_value(input, crit)) {
+// 	    return true;
+// 	}
+//     }
+//     return false;
+// }
+
+bool check_value2 (int input, std::vector<struct criterion> criteria) {
+    for (struct criterion& crit: criteria) {
+	if (check_value(input, crit.range1)) {
+	    return true;
+	}
+	if (check_value(input, crit.range2)) {
 	    return true;
 	}
     }
     return false;
 }
 
+bool single_check (int input, struct criterion crit) {
+    if (check_value(input, crit.range1) || check_value(input, crit.range2)) {
+	return true;
+    }  
+    return false;
+}
 
-void pp(std::vector<std::pair<int, int>> input) {
-    for (std::pair<int, int>& x: input) {
+
+void pp(std::vector<struct criterion> input) {
+    for (struct criterion& x: input) {
+	std::cout << x.name << std::endl;
 	std::cout << "First value: "<< std::endl;
-	std::cout << x.first << std::endl;
+	std::cout << x.range1.first << std::endl;
 	std::cout << "Second value: "<< std::endl;
-	std::cout << x.second << std::endl;
+	std::cout << x.range1.second << std::endl;
+	std::cout << "First value: "<< std::endl;
+	std::cout << x.range2.first << std::endl;
+	std::cout << "Second value: "<< std::endl;
+	std::cout << x.range2.second << std::endl;
     }
 }
 
@@ -111,9 +162,27 @@ std::vector<std::string> filter_tix (std::vector<std::string> tiks, std::vector<
     return res;
 }
 
-struct criterion {
-    std::string name;
-    std::pair<int, int> range1;
-    std::pair<int, int> range2;
-};
+bool check_column (NumericVector input, struct criterion crit) {
+    for (int i = 0; i < input.size(); i++) {
+	if (!single_check(input[i], crit)) {
+	    return false;
+	}
+    }
+    return true;
+}
 
+// [[Rcpp::export]]
+void test_mat (NumericMatrix input, std::vector<std::string> crits, int which = 0) {
+    auto criteria = parse_criteria(crits);
+    // std::cout << criteria.size() << std::endl;
+    // pp(criteria);
+    for (int i = 0; i < input.ncol(); i++) {
+	// std::cout << i << std::endl;
+	NumericVector g = input(_, i);
+	if (check_column(g, criteria[which])) {
+	    std::cout << criteria[which].name + " Work!" << std::endl;
+	} else {
+	    std::cout << criteria[which].name + " doesn't Work!" << std::endl;
+	}
+    }
+}
