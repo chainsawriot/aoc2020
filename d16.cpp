8058fe0b8f18b91d611f22798a0d722400b99dc3
chainsawriot <chainsawtiney@gmail.com>
2020-12-17 10:46:26 +0100

    day 16: Yesterday was too tired to update the code

--- d16.cpp
+++ d16.cpp
@@ -171,18 +171,131 @@ bool check_column (NumericVector input, struct criterion crit) {
     return true;
 }
 
-// [[Rcpp::export]]
-void test_mat (NumericMatrix input, std::vector<std::string> crits, int which = 0) {
+void pp_vec (std::vector<int> input) {
+    for (int& v: input) {
+	std::cout << v;
+	std::cout << ",";
+    }
+    std::cout << std::endl;
+}
+
+std::map<int, std::vector<int>> find_matching (NumericMatrix input, std::vector<std::string> crits) {
     auto criteria = parse_criteria(crits);
+    std::map<int, std::vector<int>> matching;
     // std::cout << criteria.size() << std::endl;
     // pp(criteria);
     for (int i = 0; i < input.ncol(); i++) {
 	// std::cout << i << std::endl;
 	NumericVector g = input(_, i);
-	if (check_column(g, criteria[which])) {
-	    std::cout << criteria[which].name + " Work!" << std::endl;
-	} else {
-	    std::cout << criteria[which].name + " doesn't Work!" << std::endl;
+	matching[i] = {};
+	for (int j = 0; j < criteria.size(); j++) {
+	    if (check_column(g, criteria[j])) {
+		matching[i].push_back(j);
+	    }
 	}
     }
+    return matching;
 }
+
+bool key_find(std::string key, std::vector<std::string> keys) {
+    return std::find(keys.begin(), keys.end(), key) != keys.end();
+}
+
+std::vector<int> c_setdiff(std::vector<int> x, std::vector<int> consider) {
+    std::vector<int> z;
+    for (int& v: x) {
+	if (std::find(consider.begin(), consider.end(), v) == consider.end()) {
+	    z.push_back(v);
+	}
+    }
+    return z;
+}
+
+
+std::vector<int> align (std::map<int, std::vector<int>> matching) {
+    std::vector<int> alignment(matching.size());
+    std::vector<int> consider;
+    while (consider.size() < alignment.size()) {
+	for (int val = 0; val < matching.size(); val++) {
+	    auto x = c_setdiff(matching[val], consider);
+	    if (x.size() == 1) {
+	    // first is column index; second is criterion index
+		alignment[val] = x[0];
+		consider.push_back(x[0]);
+	    }
+	}
+    }
+    return alignment;
+}
+
+// Code quality sucks. But I don;t have time and I can;t sleep.
+
+// [[Rcpp::export]]
+long int d16_2(NumericMatrix input, std::vector<std::string> crits, std::string mytix) {
+    auto criteria = parse_criteria(crits);
+    auto matching = find_matching(input, crits);
+    auto alignment =  align(matching);
+    auto mytick = explode(mytix, "[0-9]+");
+    long int sum = 1;
+    for (int i = 0; i < alignment.size(); i++) {
+	if (alignment[i] < 6) {
+	    sum = sum * std::stoi(mytick[i]); 
+	}
+    }
+    return sum;
+}
+
+/***R
+
+testdata <- "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"
+
+testcrit <- strsplit(testdata, "\n", TRUE)[[1]][1:3]
+testtix <- strsplit(testdata, "\n", TRUE)[[1]][9:12]
+
+stopifnot(check_test2(1, testcrit))
+stopifnot(!check_test2(4, testcrit))
+
+
+stopifnot(filter_tix(testtix, testcrit) == "7,3,47")
+
+testdata_crit <- c("class: 0-1 or 4-19", "row: 0-5 or 8-19", "seat: 0-13 or 16-19")
+
+testdata_nb <- "3,9,18
+15,1,5
+5,14,9"
+
+test_matrix <- matrix(as.numeric(str_split(paste0(testdata_nb, collapse=","), "[,\n]")[[1]]), nrow = 3, ncol = 3, byrow = TRUE)
+
+stopifnot(d16_1(testtix, testcrit) == 71)
+
+
+
+require(stringr)
+input16 <- readLines(here::here("data/input16.txt"))
+criteria <- input16[1:20]
+nbtix <- input16[26:length(input16)]
+yrtix <- input16[23]
+
+valid_tix <- filter_tix(nbtix, criteria)
+
+
+## 190x20
+nb_matrix <- matrix(as.numeric(str_split(paste0(valid_tix, collapse=","), ",")[[1]]), nrow = 190, ncol = 20, byrow = TRUE)
+
+d16_1(nbtix, criteria)
+
+d16_2(nb_matrix, criteria, yrtix)
+
+
+ */
